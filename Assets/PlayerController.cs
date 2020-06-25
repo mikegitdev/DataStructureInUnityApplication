310f2b75dc1f79bb642bb8c62796444bf4ae44dd
Mukhriddin Ergashaliev <miwady@mail.ru>
2020-06-25 23:33:10 +0900

    Ready one player

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public Camera camera;
+    public NavMeshAgent navMeshAgent;
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                navMeshAgent.SetDestination(hit.point);
+            }
+        }
+    }
+}
